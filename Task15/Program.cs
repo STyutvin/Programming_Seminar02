f0ad6f3fd1b5027e33511dfd8ac82a0b7d64bf63
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-11 16:10:53 +0300

    Задача №15 решена

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+        string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        Console.WriteLine("Введите число:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0 && num < 6)
+        {
+            Console.WriteLine(days[num - 1] + ". Является рабочим днем.");
+        }
+        else if (num > 5 && num < 8)
+        {
+            Console.WriteLine(days[num - 1] + ". Является выходным днем.");
+        }
+        else
+        {
+            Console.WriteLine("День недели введен неправильно.");
+        }
+    }
+}
