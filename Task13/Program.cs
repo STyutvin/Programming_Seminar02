0de4cca9b67896dd6bc761612b8261aa2daa6ce7
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-11 16:23:31 +0300

    Задача №13 решена

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        Console.Write("Введи число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        string numText = Convert.ToString(num);
+        if (numText.Length > 2)
+        {
+            Console.WriteLine("третья цифра -> " + numText[2]);
+        }
+        else
+        {
+            Console.WriteLine("-> третьей цифры нет");
+        }
+    }
+}
