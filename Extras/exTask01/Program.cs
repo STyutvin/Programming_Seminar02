6534bc2c0899d10bcc2f7f34acaad867b7534370
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-13 10:07:15 +0300

    Дополнительная задача №1 решена

--- Extras/exTask01/Program.cs
+++ Extras/exTask01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задача 1. Написать программу, которая определяет, является ли треугольник со сторонами a, b, c равнобедренным.
+        Console.Write("Введите длину стороны AB: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите длину стороны BC: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите длину стороны AC: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        if (a == b || a == c || b == c)
+        {
+            Console.WriteLine("Треугольник ABC является равнобедренным");
+        }
+        else
+        {
+            Console.WriteLine("Треугольник ABC не является равнобедренным");
+        }
+    }
+}
