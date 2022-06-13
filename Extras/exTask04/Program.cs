2e05238843887aa1b92b500a3041ce2fdf9e407e
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-13 12:40:10 +0300

    Решение дополнительной задачи №4 на текущий момент отсутствует

--- Extras/exTask04/Program.cs
+++ Extras/exTask04/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+//Дано натуральное число. Определить его максимальную цифру. Определить его минимальную цифру.
+int n = 10, min = 9, max = 0, d;
+   Console.Write("Введите натуральное число: ");
+   n = Convert.ToInt32(Console.ReadLine());
+   while (n > 0)
+   {
+      d = n % 10;
+      if (d < min)
+         min = d;
+      if (d > max)
+         max = d;
+      n = n/10;
+   }
+   Console.WriteLine($"Минимальная цифра - {min}, максимальная цифра - {max}");
+   Console.ReadKey();
+*/   
