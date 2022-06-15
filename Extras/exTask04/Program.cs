909328e31c3137cc01904b6dcf3684a6e89186ab
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-15 20:40:17 +0300

    Дополнительная задача №4 решена

--- Extras/exTask04/Program.cs
+++ Extras/exTask04/Program.cs
@@ -1,17 +1,45 @@
-﻿/*
-//Дано натуральное число. Определить его максимальную цифру. Определить его минимальную цифру.
-int n = 10, min = 9, max = 0, d;
-   Console.Write("Введите натуральное число: ");
-   n = Convert.ToInt32(Console.ReadLine());
-   while (n > 0)
-   {
-      d = n % 10;
-      if (d < min)
-         min = d;
-      if (d > max)
-         max = d;
-      n = n/10;
-   }
-   Console.WriteLine($"Минимальная цифра - {min}, максимальная цифра - {max}");
-   Console.ReadKey();
-*/   
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задача 4. Дано натуральное число, в котором все цифры различны. Определить, какая цифра расположена в нем левее: максимальная или минимальная.
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        string str = Convert.ToString(number);
+        int arrLen = str.Length;
+        int[] array = new int[arrLen];
+        for (int i = arrLen - 1; i >= 0; i--)
+        {
+            array[i] = number % 10;
+            number /= 10;
+        }
+        int max = array[0];
+        int min = array[0];
+        int maxIndex = 0;
+        int minIndex = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+                minIndex = i;
+            }
+        }
+        if (maxIndex > minIndex)
+        {
+            Console.WriteLine($"Во введенном числе {str} максимальная цифра {max} находится правее минимальной цифры {min}");
+        }
+        else
+        {
+            Console.WriteLine($"Во введенном числе {str} максимальная цифра {max} находится левее минимальной цифры {min}");
+        }
+    }
+}
